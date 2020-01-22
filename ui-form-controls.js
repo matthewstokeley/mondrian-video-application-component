b7f13ec2dcb82cac1c16549dfaaca66b41e9a899
Matthew <Stokely@Guests-iMac.local>
2020-01-22 13:22:54 -0500

    init dotfiles

---
+++ ui-form-controls.js
@@ -0,0 +1,17 @@
+<!-- form controls -->
+
+<script template>
+
+	export default ( props ) => `<button>
+
+		<a href=""
+		   rel=""
+		   v:model=""
+		   v:onclick=""
+		   aria-pressed="false"
+		   data-direction="{props.direction}"
+		   data-state="inactive"
+		></a>
+	</button>`
+
+</script>
