33f81dbd1af4317f76b9713d083966b4dda5f4c3
Matthew <Stokely@Guests-iMac.local>
2020-01-27 14:54:41 -0500

    scaffold machinmachine formatting

---
+++ bootstrap-typescript-support.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Leibowitz Branding and Design
+# Typescript Support
+#
+# @version 0.0.1
---
+++ tests/Styled-Carousel-Component.spec.js
@@ -0,0 +1,7 @@
+test( 'exoect 2+2', () => {
+
+	expect(2+2).toBe(4)
+
+} )
+
+
